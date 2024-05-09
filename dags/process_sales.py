03240415438b288c4777a481f441a190e84607ae
Nazar Klypych <klypych.nazar@gmail.com>
2024-05-09 09:29:04 +0300

    renamed tasks, using SimpleHttpOperator

--- dags/process_sales.py
+++ dags/process_sales.py
@@ -1,11 +1,10 @@
+import json
 import os
 from datetime import datetime
 
-import httpx
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.models import Variable
-from httpx import Response
 from airflow.hooks.base import BaseHook
 
 
@@ -26,35 +25,6 @@ default_args = {
 }
 
 
-def trigger_task_1(date: str) -> bool:
-    raw_dir: str = os.path.join(BASE_DIR, 'raw', 'sales', date)
-    resp: Response = httpx.post(
-        url=get_server_url('extract_from_api'),
-        json={
-            "date": date,
-            "raw_dir": raw_dir
-        }
-    )
-    if resp.status_code == 201:
-        return True
-    return False
-
-
-def trigger_task_2(date: str) -> bool:
-    raw_dir: str = os.path.join(BASE_DIR, 'raw', 'sales', date)
-    stg_dir: str = os.path.join(BASE_DIR, 'stg', 'sales', date)
-    resp: Response = httpx.post(
-        url=get_server_url('json_to_avro'),
-        json={
-            "raw_dir": raw_dir,
-            "stg_dir": stg_dir
-        }
-    )
-    if resp.status_code == 201:
-        return True
-    return False
-
-
 with DAG(
         dag_id='process_sales',
         start_date=datetime(2022, 8, 9),
@@ -64,16 +34,30 @@ with DAG(
         default_args=default_args
 ) as dag:
 
-    task_1 = PythonOperator(
+    extract_data_from_api = SimpleHttpOperator(
         task_id='extract_data_from_api',
-        python_callable=trigger_task_1,
-        op_kwargs={'date': '{{ ds }}'}
+        method='POST',
+        data=json.dumps({
+            'date': '{{ ds }}',
+            'raw_dir': os.path.join(BASE_DIR, 'raw', 'sales', '{{ ds }}')
+        }),
+        headers={'Content-Type': 'application/json'},
+        http_conn_id='extract_from_api',
+        response_check=lambda response: response.status_code == 201,
+        log_response=True
     )
 
-    task_2 = PythonOperator(
+    convert_to_avro = SimpleHttpOperator(
         task_id='convert_to_avro',
-        python_callable=trigger_task_2,
-        op_kwargs={'date': '{{ ds }}'}
+        method='POST',
+        data=json.dumps({
+            'raw_dir': os.path.join(BASE_DIR, 'raw', 'sales', '{{ ds }}'),
+            'stg_dir': os.path.join(BASE_DIR, 'stg', 'sales', '{{ ds }}')
+        }),
+        headers={'Content-Type': 'application/json'},
+        http_conn_id='json_to_avro',
+        response_check=lambda response: response.status_code == 201,
+        log_response=True
     )
 
-    task_1 >> task_2
+    extract_data_from_api >> convert_to_avro
